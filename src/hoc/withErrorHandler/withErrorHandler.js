6b6ffbbf855390e0ff285ca6b5481530e9d77e24
Ross King <ross@design-monkey.co.uk>
2019-10-25 15:51:08 +0100

    App in to class component, remove error handler interceptors on unmount

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,17 +7,22 @@ const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
     constructor () {
       super();
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({ error: error });
       });
     }
     
     state = {
       error: null
+    }
+
+    componentWillUnmount () {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
